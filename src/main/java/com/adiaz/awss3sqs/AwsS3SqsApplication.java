62aeea174bfb3a2bfe38dfb6a0abca689d2a2b9c
Antonio Diaz Arroyo <antonio.diaz.arroyo@gmail.com>
2023-11-09 10:47:33 +0100

    first commit

---
+++ src/main/java/com/adiaz/awss3sqs/AwsS3SqsApplication.java
@@ -0,0 +1,13 @@
+package com.adiaz.awss3sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3SqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3SqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adiaz/awss3sqs/AwsS3SqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adiaz.awss3sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3SqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
