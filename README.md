9f5eb96577e239b15879d38a0360dc806ee9292d
Antonio Diaz Arroyo <antonio.diaz.arroyo@gmail.com>
2023-11-11 10:02:18 +0100

    add bucket file dashboard

--- README.md
+++ README.md
@@ -3,7 +3,17 @@
 <!-- TOC -->
   * [Links](#links)
 <!-- TOC -->
+* Shows a list of all object of a S3 bucket.
+
 
 ## Links
-[Getting started with Spring Boot on AWS](https://aws.amazon.com/blogs/opensource/getting-started-with-spring-boot-on-aws-part-1/)  
-[GitHub Spring Cloud AWS](https://github.com/awspring/spring-cloud-aws)
+[Getting started with Spring Boot on AWS](https://aws.amazon.com/blogs/opensource/getting-started-with-spring-boot-on-aws-part-1/)    
+[GitHub Spring Cloud AWS](https://github.com/awspring/spring-cloud-aws)  
+[AWS Spring Reference](https://docs.awspring.io/spring-cloud-aws/docs/3.0.3/reference/html/index.html)  
+
+
+
+
+* Make bucket public
+```
+```
--- src/main/java/com/adiaz/awss3sqs/controllers/HomeController.java
+++ src/main/java/com/adiaz/awss3sqs/controllers/HomeController.java
@@ -2,24 +2,51 @@ package com.adiaz.awss3sqs.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.util.List;
 
 @Controller
 @Slf4j
 public class HomeController {
 
-  @Value("${cloud.aws.credentials.profile-name}")
-  String profileName;
 
-  @GetMapping("/hello")
-  public ModelAndView home() {
-    log.info("profileName -> {}", profileName);
-    return new ModelAndView("hello");
+  private final String profileName;
+
+  @Value("${custom.bucket-name}")
+  private final String bucketName;
+
+  private final S3Client s3Client;
+
+  private final String bucketLocation;
+
+
+  public HomeController(@Value("${cloud.aws.credentials.profile-name}") String  profileName,
+                        @Value("${custom.bucket-name}")String bucketName,
+                        S3Client s3Client) {
+    this.profileName = profileName;
+    this.bucketName = bucketName;
+    this.s3Client = s3Client;
+    String s3Region = this.s3Client
+            .getBucketLocation(GetBucketLocationRequest.builder().bucket(bucketName).build())
+            .locationConstraintAsString();
+    this.bucketLocation = String.format("https://%s.s3.%s.amazonaws.com", bucketName, s3Region);
   }
 
-
+  @GetMapping
+  public ModelAndView home() {
+    ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder().bucket(bucketName).build();
+    ListObjectsV2Response listObjectsV2Response = s3Client.listObjectsV2(listObjectsV2Request);
+    List<S3Object> contents = listObjectsV2Response.contents();
+    ModelAndView modelAndView = new ModelAndView("dashboard");
+    modelAndView.addObject("message", "Spring Boot with AWS");
+    modelAndView.addObject("bucketName", bucketName);
+    modelAndView.addObject("availableFiles", contents);
+    modelAndView.addObject("bucketLocation", bucketLocation);
+    return modelAndView;
+  }
 }
